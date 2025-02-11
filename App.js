ba74fdd5418abef12e002f803a9fa1871809a602
Pham Chien <phamchien5870@gmail.com>
2025-02-11 10:16:51 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [bgColor, setBgColor] = useState('white');
+
+  const green = () => {
+    setBgColor('green');
+  };
+  const blue = () => {
+    setBgColor('blue');
+  };
+  const brown = () => {
+    setBgColor('brown');
+  };
+  const yellow = () => {
+    setBgColor('yellow');
+  };
+  const red = () => {
+    setBgColor('red');
+  };
+  const black = () => {
+    setBgColor('black');
+  };
+ 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {backgroundColor: bgColor} ]}>
+      <TouchableOpacity style={[styles.button, {backgroundColor:'green'}]}  onPress={green} >
+        <Text style={styles.buttonText}>Green</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, {backgroundColor:'blue'}]} onPress={blue}>
+        <Text style={styles.buttonText}>Blue</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, {backgroundColor:'brown'}]} onPress={brown}>
+        <Text style={styles.buttonText}>Brown</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, {backgroundColor:'yellow'}]} onPress={yellow}>
+        <Text style={[styles.buttonText, {color:'black'}]}>Yellow</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, {backgroundColor:'red'}]} onPress={red}>
+        <Text style={styles.buttonText}>Red</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, {backgroundColor:'black'}]} onPress={black}>
+        <Text style={styles.buttonText}>Black</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +51,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    
+  },
+  counterText: {
+    fontSize: 48,
+    marginBottom: 20,
+  },
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginVertical: 10,
+
+  
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
